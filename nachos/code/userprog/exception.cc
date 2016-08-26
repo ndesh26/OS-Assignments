b213081e8b239c6853382cf3df75fd9268c3e5de
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2016-08-27 00:03:38 +0530

    exception: implement GetReg syscall
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -157,6 +157,13 @@ ExceptionHandler(ExceptionType which)
        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+    }
+    else if ((which == SyscallException) && (type == SYScall_GetReg)) {
+       machine->WriteRegister(2, machine->ReadRegister(machine->ReadRegister(4)));
+       // Advance program counters.
+       machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+       machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+       machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
     } else {
 	printf("Unexpected user mode exception %d %d\n", which, type);
 	ASSERT(FALSE);
