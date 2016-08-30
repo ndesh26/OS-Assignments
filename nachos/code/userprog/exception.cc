43c178ae187b6c0a1c4fa205fab7a98596155cb7
Nayan Deshmukh <nayan26deshmukh@gmail.com>
2016-08-30 18:55:46 +0530

    exception: implement GetPPID syscall
    Signed-off-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -243,7 +243,15 @@ ExceptionHandler(ExceptionType which)
         machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
         machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
     }
-    else {
+    else if ((which == SyscallException) && (type == SYScall_GetPPID)) {
+        machine->WriteRegister(2, currentThread->getPpid());
+
+        // Advance program counter
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+    }
+   else {
 	printf("Unexpected user mode exception %d %d\n", which, type);
 	ASSERT(FALSE);
     }
